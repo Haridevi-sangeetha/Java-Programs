fad9e17f83acac230550a1d20697e7a473602a88
Haridevi <hari93609073@gmail.com>
2026-06-05 21:10:48 +0530

    Add files via upload

---
+++ String/Remove Spaces from String.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String str = "J a v a P r o g r a m";
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+    }
+}
