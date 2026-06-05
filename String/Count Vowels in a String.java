0982f47709a22a818bae4ac0668a36f9d667c2be
Haridevi <hari93609073@gmail.com>
2026-06-05 21:13:12 +0530

    Add files via upload

---
+++ String/Count Vowels in a String.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                count++;
+            }
+        }
+
+        System.out.println("Vowels: " + count);
+    }
+}
